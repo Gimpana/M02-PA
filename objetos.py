c998219c6568be0a495ae88b7c4b42bc536be6e2
Gimpana <ulisesvs21@gmail.com>
2021-01-20 13:02:09 +0100

    La recursividad y primeras practicas con objetos

--- objetos.py
+++ objetos.py
@@ -2,8 +2,17 @@ import turtle
 
 tortugita = turtle.Turtle()
 otraTortugita = turtle.Turtle()
+lentorra = turtle.Turtle()
 
 tortugita.fd(50)
+tortugita.shape("turtle")
+tortugita.color("blue")
+tortugita.speed(5)
 
+otraTortugita.color("green")
 otraTortugita.left(90)
 otraTortugita.fd(50)
+otraTortugita.speed(5)
+
+lentorra.speed(1)
+lentorra.fd(50)
